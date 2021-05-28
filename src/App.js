fd810ae5e7dbc81073677dfb36067a828a7cfb3e
nuevo-rama <ramiromelero@gmail.com>
2021-05-27 23:47:03 -0300

    base

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { Provider } from "react-redux";
 import store from "./store";
-import Jugadores from "./components/Jugadores";
+import Modelos from "./components/Modelos";
+import ItemsSeleccionados from "./components/ItemsSeleccionados";
+import "./styles/styles.css";
 
 
 const App = () => ( //COMPONENTE PRINCIPAL
   <Provider store = {store}>
     <main>
       <h1>Tabla de Selección</h1>
-      <Jugadores/>
-      <EquipoSeleccionado/>
+      <Modelos/>
+      <ItemsSeleccionados/>
       
 
 
