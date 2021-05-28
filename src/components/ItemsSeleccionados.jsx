956861f5f355083c6f3528e6490c0f8eaeefee10
nuevo-rama <ramiromelero@gmail.com>
2021-05-27 23:49:49 -0300

    seleccionados

---
+++ src/components/ItemsSeleccionados.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Lija from "./Lija";
+import Tabla from "./Tabla";
+
+const ItemsSeleccionados = () => (
+    <section>
+        <Lija />
+        <Tabla />
+    </section>
+)
+
+
+export default ItemsSeleccionados;
