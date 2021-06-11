1443ecdaa84ce1c578e974761a00e9e0e937014e
nuevo-rama <you@example.com>
2021-06-10 22:35:34 -0300

    completo el componente

--- src/components/Alumnos.jsx
+++ src/components/Alumnos.jsx
@@ -1,49 +1,123 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Avatar from '@material-ui/core/Avatar';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
 import { connect } from "react-redux"; //me conecto al store para poder leer la data
 
-const Alumnos = ({alumnos, agregarDia1, agregarDia2}) => (
-    <section>
-        <h2>Alumnos</h2>
-        <div className= "contenedor-modelos">
+const useStyles = makeStyles((theme) => ({
+    text: {
+      padding: theme.spacing(2, 2, 0),
+    },
+    paper: {
+      paddingBottom: 50,
+    },
+    list: {
+      marginBottom: theme.spacing(2),
+    },
+    subheader: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    appBar: {
+      top: 'auto',
+      bottom: 0,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    fabButton: {
+      position: 'absolute',
+      zIndex: 1,
+      top: -30,
+      left: 0,
+      right: 0,
+      margin: '0 auto',
+    },
+  }));
+
+  function Alumnos({alumnos, agregarDia1, agregarDia2}) {
+    const classes = useStyles();
+  
+    return (
+  
+    <React.Fragment>
+        <CssBaseline />
+        <Paper square className={classes.paper}>
+          <Typography className={classes.text} variant="h5" gutterBottom>
+            Alumnos
+          </Typography>
+          <List className={classes.list}>
             {
-                alumnos.map ( m => (
-                    <article className= "modelo" key= {m.id}>
-                <img src={m.foto} alt={m.nombre}/>
-                <h3>{m.nombre}</h3>
-                <div>
-                    <button onClick = { () => agregarDia1 (m)}>Clase Día 1</button>
-                    <button onClick = { () => agregarDia2 (m)}>Clase Día 2</button>
-                </div>
+            alumnos.map ( m => (
+              <React.Fragment key={m.id}>
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar alt="Profile Picture" src={m.foto} />
+                  </ListItemAvatar>
+                  <ListItemText primary={m.nombre} secondary={m.calificaciones}/>
+                  <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group" size="small">
+                    <Button onClick = { () => agregarDia1 (m)}>Classroom 1</Button>
+                    <Button onClick = { () => agregarDia2 (m)}>Classroom 2</Button>
+                  </ButtonGroup>                    
+                </ListItem>
+              </React.Fragment>
+            ))}
+          </List>
+        </Paper>
+        <AppBar position="fixed" color="primary" className={classes.appBar}>
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="open drawer">
+              <MenuIcon />
+            </IconButton>
+            <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+              <AddIcon />
+            </Fab>
+            <div className={classes.grow} />
+            <IconButton color="inherit">
+              <SearchIcon />
+            </IconButton>
+            <IconButton edge="end" color="inherit">
+              <MoreIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </React.Fragment>
 
-            </article>
-                ))
-            }
-            
-        </div>
-    </section>
-
-)
-
-
-const mapStateToProps = state => ({ //mapStateToProps mapea lo que tengo en el estado y lo convierte en propiedades 
-    alumnos : state.alumnos
-});
-
-const mapDispatchToProps = dispatch => ({ //mapDispatchToProps mapea las funciones y las convierte en propiedades
-    agregarDia1 (alumno) {
-        dispatch ({                         // objeto action
-                type: "AGREGAR_DIA1",    //tipo de action
-                alumno                   //objeto
-            })
-        },
-    agregarDia2 (alumno) {
-        dispatch ({                         // objeto action
-                type: "AGREGAR_DIA2",    //tipo de action
-                alumno                   //objeto
-            })
-        }
+)};
+    const mapStateToProps = state => ({ //mapStateToProps mapea lo que tengo en el estado y lo convierte en propiedades 
+        alumnos : state.alumnos
     });
-
-export default connect(mapStateToProps, mapDispatchToProps)(Alumnos); 
-
-
+    
+    const mapDispatchToProps = dispatch => ({ //mapDispatchToProps mapea las funciones y las convierte en propiedades
+        agregarDia1 (alumno) {
+            dispatch ({                         // objeto action
+                    type: "AGREGAR_DIA1",    //tipo de action
+                    alumno                   //objeto
+                })
+            },
+        agregarDia2 (alumno) {
+            dispatch ({                         // objeto action
+                    type: "AGREGAR_DIA2",    //tipo de action
+                    alumno                   //objeto
+                })
+            }
+        });
+    
+    export default connect(mapStateToProps, mapDispatchToProps)(Alumnos); 
