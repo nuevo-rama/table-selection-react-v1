c4ca4100f9bea8a9f72e069c001f8ef9ec439ced
nuevo-rama <you@example.com>
2021-06-15 20:10:40 -0300

    aplico css baseline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import { Provider } from "react-redux";
 import store from "./store";
 import Alumnos from "./components/Alumnos";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ItemsSeleccionados from "./components/ItemsSeleccionados";
+
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import "./styles/styles.css";
 
 
 const App = () => ( //COMPONENTE PRINCIPAL
   <Provider store = {store}>
-    <main>
+    <React.Fragment>
+      <CssBaseline />
       <Header/>
       
       <Alumnos/>
@@ -17,11 +22,9 @@ const App = () => ( //COMPONENTE PRINCIPAL
       <ItemsSeleccionados/>
       
       <Footer/>
-      
-
-
-    </main>
-    </Provider>
+    </React.Fragment>
+    
+  </Provider>
   
 ) 
 
